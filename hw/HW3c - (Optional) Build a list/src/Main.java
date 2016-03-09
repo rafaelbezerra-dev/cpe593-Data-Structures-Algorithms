88c8b282b7f0bca12cc4e754c1a8eafafad4fc17
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-03-09 18:51:37 -0500

    hwHW4b - Double Linked List

--- hw/HW3c - (Optional) Build a list/src/Main.java
+++ hw/HW3c - (Optional) Build a list/src/Main.java
@@ -1,5 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
@@ -39,11 +43,61 @@ public class Main {
 		return res;
 	}
 
+	public static int[] runLessRam(int[] x, int[] y) {
+		int[] res;
+		int count = x.length;
+
+		for (int i = 0; i < y.length; i++) {
+			for (int j = 0; j < x.length; j++) {
+				if (x[j] == 0)
+					if (x[j] == y[i])
+						break;
+					else
+						continue; 
+				
+				if (x[j] % y[i] == 0) {
+					for (int l = j; l < x.length; l += y[i]) {
+						if (x[l] == 0)
+							continue;
+						x[l] = 0;
+						count--;
+					}
+					break;
+				}
+			}
+		}
+
+		res = new int[count];
+		int res_index = 0;
+		for (int i = 0; i < x.length; i++) {
+			if (x[i] != 0) {
+				res[res_index++] = x[i];
+			}
+		}
+		return res;
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
+		/*
+		 * THIS CODE WAS TESTED WITH LISTS UP TO 10^9.
+		 * BEFORE RUNNIG, YOU SHOULD CHANGE THE MAXIMUM
+		 * JAVA HEAP SIZE TO 12 GB IF YOU WANT TO LISTS
+		 * OF THAT SIZE.
+		 * 
+		 * IF YOU DO NOT WANT TO USE THAT MUCH RAM, 
+		 * YOU MAY ALSO USE THE METHOD runLessRam()
+		 * BUT IT IS WAY SLOWER AND YOU STILL NEED TO
+		 * USE AT LEAST 6 GB TO RUN IT FOR LISTS WITH
+		 * SIZES OF 10^9. 
+		 * */
+		
 		System.out.println("Max Memory: " + (Runtime.getRuntime().maxMemory()/(Math.pow(2, 20))) + " MB");
 		
-		Scanner s = new Scanner(new FileReader("HW.dat"));
 		final String pattern = "Cases:";
+		final String source = "HW.dat";
+		Scanner s = new Scanner(new FileReader(source));
+		StringBuilder csv = new StringBuilder("list_size,miliseconds\n");
+		
 		int cases = 0;
 		String firstLine = s.nextLine();
 		if (firstLine.contains(pattern)) {
@@ -67,10 +121,23 @@ public class Main {
 			long sum = 0;
 			for (int j = 0; j < res.length; j++)
 				sum += res[j];
-			
-			System.out.println(res.length + " " + sum + " " + (System.currentTimeMillis() - startTime) );
+			long elapsedTime = System.currentTimeMillis() - startTime;
+			System.out.println(res.length + " " + sum + " " + elapsedTime);
+			csv.append(arr.length)
+				.append(",")
+				.append(elapsedTime)
+				.append("\n");
+			System.gc();
 		}
-
 		s.close();
+		
+		try {
+			System.out.print("Writing output to file...");
+			Files.write(Paths.get(source.replace(".dat", "") + ".csv"), csv.toString().getBytes());
+			System.out.println(" done.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
