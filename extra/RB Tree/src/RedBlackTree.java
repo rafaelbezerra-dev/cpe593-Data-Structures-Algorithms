dc7d8a2a89b845ed8b9a4058484732679f888a48
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2016-03-12 15:21:40 -0500

    ..

--- extra/RB Tree/src/RedBlackTree.java
+++ extra/RB Tree/src/RedBlackTree.java
@@ -2,9 +2,14 @@
 public class RedBlackTree {
 
 	public static class RBTree {
+		public static enum Colors {
+			RED, BLACK
+		}
+
 		public static class Node {
 			Node left, right, parent;
 			int val;
+			Colors color;
 
 			public Node(Node left, Node right, Node parent, int val) {
 				// super();
@@ -30,8 +35,70 @@ public class RedBlackTree {
 				else
 					return g.left;
 			}
-		
-			
+
+			void swapChildForParent() {
+				this.parent = this;
+			}
+
+			void insert_case1() {
+				if (this.parent == null) {
+					this.color = Colors.BLACK;
+				} else {
+					if (this.parent.color == Colors.BLACK) {
+						return; /* Tree is still valid */
+					} else {
+						Node u = uncle();
+						Node g;
+
+						if ((u != null) && (u.color == Colors.RED)) {
+							this.parent.color = Colors.BLACK;
+							u.color = Colors.BLACK;
+							g = grandparent();
+							g.color = Colors.RED;
+							insert_case1();
+						} else {
+							g = grandparent();
+
+							if ((this == this.parent.right) && (this.parent == g.left)) {
+								rotateLeft(g);
+
+								// this = this.left;
+								this.left.swapChildForParent();
+
+							} else if ((this == this.parent.left) && (this.parent == g.right)) {
+								rotateRight(g);
+
+								// this = this.right;
+								this.right.swapChildForParent();
+							}
+							g = grandparent();
+
+							this.parent.color = Colors.BLACK;
+							g.color = Colors.RED;
+							if (this == this.parent.left)
+								rotateRight(g);
+							else
+								rotateLeft(g);
+						}
+					}
+				}
+			}
+
+			void rotateLeft(Node g) {
+				Node saved_p = g.left;
+				Node saved_left_n = this.left;
+				g.left = this;
+				this.left = saved_p;
+				saved_p.right = saved_left_n;
+			}
+
+			void rotateRight(Node g) {
+				Node saved_p = g.right;
+				Node saved_right_n = this.right;
+				g.right = this;
+				this.right = saved_p;
+				saved_p.left = saved_right_n;
+			}
 		}
 	}
 
